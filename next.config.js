9b9c477952ca398ac36032f44396645aa1522a94
xgmak94 <xgmak94@gmail.com>
2022-08-08 19:42:24 -0700

    Configuring tailwind

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['raw.githubusercontent.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
