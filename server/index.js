c737588f3912b29760bec7d17eec5a6bf04bc9be
xgmak94 <xgmak94@gmail.com>
2022-08-08 19:27:11 -0700

    Setup server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(morgan('tiny'));
+
+// app.use('/', routes);
+
+let port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
